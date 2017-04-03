daa2c06a6814ce26cd41fed8c168247da03723d5
Kyung-Tae <speed9640@naver.com>
2017-04-03 14:43:25 +0900

    주석 달아쩌 뿌잉뿌잉

--- app/src/main/java/com/example/kennethelee/nammuk/MainActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.kennethelee.nammuk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+//This is for a test(KT's writings)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
