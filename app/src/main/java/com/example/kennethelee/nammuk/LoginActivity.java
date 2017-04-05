392858192430b711533fb89d0771eaa23f017a0f
KT <speed9640@naver.com>
2017-04-05 23:02:39 +0900

    commit test

--- app/src/main/java/com/example/kennethelee/nammuk/LoginActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk/LoginActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+//merge checking
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
