824d61e0b8ed208b47f12432707768792f35e68a
KT <speed9640@naver.com>
2017-04-07 18:25:16 +0900

    commit for firebase

--- app/src/main/java/com/example/kennethelee/nammuk/LoginActivity.java
+++ app/src/main/java/com/example/kennethelee/nammuk/LoginActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+//Commit test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
